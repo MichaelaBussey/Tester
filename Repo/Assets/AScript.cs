2a90be42ba0d8b53c8ee4b549b80d24906b50bc4
MichaelaBussey <80414418+MichaelaBussey@users.noreply.github.com>
2021-03-10 18:57:28 +0200

    First Script

---
+++ Repo/Assets/AScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AScript : MonoBehaviour
+{
+    private int Song = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
